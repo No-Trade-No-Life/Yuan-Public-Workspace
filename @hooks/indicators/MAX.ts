573c442455b4e2cdb886047a2ef2379cde3996e3
Zheng Chen <zheng.chen@no-trade-no.life>
2023-08-04 00:35:29 +0800

    Double-MA, Dual-Thrust,R-Breaker (#1)

--- @hooks/indicators/MAX.ts
+++ @hooks/indicators/MAX.ts
@@ -1,13 +1,13 @@
 /**
- * 计算滑动窗口最小值 (Rolling Min)
+ * 计算滑动窗口最大值 (Rolling Max)
  *
  * 利用了单调队列，均摊时间复杂度为 O(n)
  *
  * @param source 输入数据源
  * @param period 周期 (>0)
  */
-export const useMIN = (source: Series, period: number) => {
-  const MIN = useSeries(`MIN(${source.name},${period})`, source, {
+export const useMAX = (source: Series, period: number) => {
+  const MAX = useSeries(`MAX(${source.name},${period})`, source, {
     display: "line",
   });
   // 单调队列: 保存当前窗口期内，比最新的元素小的元素的索引
@@ -15,10 +15,10 @@ export const useMIN = (source: Series, period: number) => {
   useEffect(() => {
     const i = source.length - 2;
     if (i < 0) return;
-    // 从栈顶开始，移除所有大于当前值的元素
+    // 从栈顶开始，移除所有小于当前值的元素
     while (
       queue.current.length > 0 &&
-      source[queue.current[queue.current.length - 1]] >= source[i]
+      source[queue.current[queue.current.length - 1]] <= source[i]
     ) {
       queue.current.pop();
     }
@@ -33,10 +33,10 @@ export const useMIN = (source: Series, period: number) => {
     const i = source.length - 1;
     if (i < 0) return;
     // 队首元素即为当前窗口期内的最小值
-    MIN[i] = Math.min(
+    MAX[i] = Math.max(
       source[source.length - 1],
-      source[queue.current[0]] || Infinity
+      source[queue.current[0]] || -Infinity
     );
   });
-  return MIN;
+  return MAX;
 };
