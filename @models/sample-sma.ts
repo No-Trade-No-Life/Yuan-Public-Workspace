a325865efb51c01b1f33e5e53059e4a871a4835c
Siyuan Wang <c.one@thrimbda.com>
2023-08-03 18:42:16 +0800

    chore(sma): add comments

--- @models/sample-sma.ts
+++ @models/sample-sma.ts
@@ -2,19 +2,25 @@
 import { useSMA } from "@hooks";
 
 export default () => {
+  // 设定参数
   const datasource_id = useParamString("数据源ID");
   const product_id = useParamString("品种ID");
   const period_in_sec = useParamNumber("周期");
 
+  // 使用收盘价序列
   const { close: c } = useOHLC(datasource_id, product_id, period_in_sec);
+  // NOTE: 使用当前 K 线的上一根 K 线的收盘价，保证策略在 K 线结束时才会执行
   const idx = c.length - 2;
 
+  // 使用 20，60 均线
   const sma20 = useSMA(c, 20);
   const sma60 = useSMA(c, 60);
 
+  // 设置仓位管理器
   const pL = useSinglePosition(product_id, PositionVariant.LONG);
   const pS = useSinglePosition(product_id, PositionVariant.SHORT);
 
+  // 金叉开多平空
   useEffect(() => {
     if (idx < 60) {
       return;
@@ -31,6 +37,7 @@ export default () => {
     }
   }, [idx]);
 
+  // 死叉开空平多
   useEffect(() => {
     if (idx < 60) {
       return;
