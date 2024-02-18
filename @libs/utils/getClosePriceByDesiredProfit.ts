68102915843f2701d6b0b784a2d55fab31ae4f1c
CZ <zheng.chen@no-trade-no.life>
2024-02-18 08:48:35 +0800

    update data model of IProduct

--- @libs/utils/getClosePriceByDesiredProfit.ts
+++ @libs/utils/getClosePriceByDesiredProfit.ts
@@ -9,7 +9,6 @@
  * @param currency - 账户货币
  * @param quotes - 市场报价
  * @returns - 目标平仓价
- * @see https://tradelife.feishu.cn/wiki/wikcnRNzWSF7jtkH8nGruaMhhlh
  */
 export const getClosePriceByDesiredProfit = (
   product: IProduct,
@@ -22,23 +21,24 @@ export const getClosePriceByDesiredProfit = (
 ) => {
   const variant_coefficient = variant === PositionVariant.LONG ? 1 : -1;
   const cross_product_exchange_rate =
-    product.base_currency !== currency
+    product.quote_currency !== currency
       ? (variant === PositionVariant.LONG
-          ? quotes(`${product.base_currency}${currency}`)?.bid
-          : quotes(`${product.base_currency}${currency}`)?.ask) ?? 1
+          ? quotes(`${product.quote_currency}${currency}`)?.bid
+          : quotes(`${product.quote_currency}${currency}`)?.ask) ?? 1
       : 1;
 
   const beta =
-    desiredProfit / (variant_coefficient * volume * (product.value_speed ?? 1));
+    desiredProfit /
+    (variant_coefficient *
+      volume *
+      (product.value_scale ?? 1) *
+      (product.value_scale_unit ? 1 / openPrice : 1));
 
-  if (!product.is_underlying_base_currency) {
-    return openPrice + beta / cross_product_exchange_rate;
+  if (product.quote_currency === currency) {
+    return beta + openPrice;
   }
-  if (product.quoted_currency === currency) {
-    return openPrice + beta;
+  if (product.base_currency && product.base_currency === currency) {
+    return openPrice / (1 - beta);
   }
-  if (beta >= cross_product_exchange_rate) {
-    return NaN;
-  }
-  return openPrice / (1 - beta / cross_product_exchange_rate);
+  return openPrice + beta / cross_product_exchange_rate;
 };
